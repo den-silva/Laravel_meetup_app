1494475ea67fc4901a333a29a4de91da340817fd
Denilson <dmurilosilva@hotmail.com>
2021-07-30 17:58:23 -0300

    Curso Laravel - Rotas e Views - #03

--- Meetup_app/routes/web.php
+++ Meetup_app/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/product', function () {
+    return view('product');
+});
