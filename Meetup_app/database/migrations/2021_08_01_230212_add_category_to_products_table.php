0e4534b5500acaef663e6cf605d29704ea02f3c8
Denilson <dmurilosilva@hotmail.com>
2021-08-01 20:06:11 -0300

    Curso Laravel - Avançando em migrations do Laravel - #12

--- Meetup_app/database/migrations/2021_08_01_230212_add_category_to_products_table.php
+++ Meetup_app/database/migrations/2021_08_01_230212_add_category_to_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class AddCategoryToProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 50);
-            $table->integer('quantity');
-            $table->text('description');
-            $table->timestamps();
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('category', 50);
         });
     }
 
@@ -29,6 +25,8 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('category', 50);
+        });
     }
 }
