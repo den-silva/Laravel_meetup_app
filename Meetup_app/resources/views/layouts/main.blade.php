6a9153cf982eac6781f88930924b3037b18c01de
Denilson <dmurilosilva@hotmail.com>
2021-08-11 10:16:00 -0300

    Curso Laravel - Finalizando a Home - #14

--- Meetup_app/resources/views/layouts/main.blade.php
+++ Meetup_app/resources/views/layouts/main.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>@yield('title')</title>
-        <link rel="stylesheet" href="css/styles.css">
         <script src="js/scripts.js"></script>
 
         <!-- FONTES DO GOOGLE -->
@@ -15,37 +14,42 @@
         <!-- BOOTSTRAP -->
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        
+        <!-- CSS CUSTOMIZÁVEL -->
+        <link rel="stylesheet" href="css/styles.css">
 
     </head>
     <body>
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="collapse navbar-collapse" id="navbar">
-                    <a href="/" class="navbar-brand">
-                        <img src="img/logo.jpg" alt="MeetUp App">
-                    </a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Eventos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/events/create" class="nav-link">Criar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="nav-link">Entrar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/contacts/create" class="nav-link">Cadastro</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="/products/create" class="nav-link">Produtos</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <div class="collapse navbar-collapse" id="navbar">
+                        <a href="/" class="navbar-brand">
+                            <img src="img/logo.jpg" alt="MeetUp App">
+                        </a>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Eventos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/events/create" class="nav-link">Criar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Entrar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/contacts/create" class="nav-link">Cadastro</a>
+                            </li>
+    
+                            <li class="nav-item">
+                                <a href="/products/create" class="nav-link">Produtos</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            
         </header>
-        <img src="img/palestra.jpg" alt="">
 
         @yield('content')
         <footer>
--- Meetup_app/resources/views/welcome.blade.php
+++ Meetup_app/resources/views/welcome.blade.php
@@ -1,7 +1,34 @@
 @extends('layouts.main')
 @section('title', 'MeetUp App')
 @section('content')
-    @foreach ($events as $event)
-        <p>{{ $event->title }} -- {{ $event->description }}</p>
-    @endforeach
+    <div id="search-container" class="col-md-12">
+        <div class="container">
+            <h1>Pequise um evento</h1>
+            <form action="">
+                <input type="text" id="search" name="search" class="form-control" placeholder="buscar...">
+            </form>
+        </div>
+    </div>
+<div class="container">
+    <div id="events-container" class="col-md-12">
+        <h2>Próximos Eventos</h2>
+        <p class="subtitle">Veja os próximos eventos</p>
+        <div id="cards-container" class="row">
+            @foreach ($events as $event)
+            <div class="card col-md-3">
+                <img src="/img/evento01.jpg" alt="{{ $event->title }}">
+                <div class="card-body">
+                    <div class="card-date">01/01/2021</div>
+                    <h5 class="card-title">{{ $event->title }}</h5>
+                    <p class="card-participants "> x participantes </p>
+                    <a href="#" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+                
+            @endforeach
+        </div>
+    </div>
+
+</div>
+
 @endsection
